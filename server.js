a8556e34ae3850f85534ff1a8752184867916c93
Antonio Gabriel Miranda <antoniogabrielti@gmail.com>
2017-09-27 15:23:55 -0300

    Configurado Servidor Web Node

---
+++ server.js
@@ -0,0 +1,14 @@
+
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req,res) {
+
+  if(req.url==='/'){
+    res.writeHead(200);
+    res.end(fs.readFileSync('public/index.html'));
+    
+  }
+
+}).listen(3000);
+console.log('Servidor iniciado em localhost:3000. Ctrl+C para encerrar…');
